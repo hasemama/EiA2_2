81c718d2083aa4fc47b4e977a566f0d84d25fb6d
denni <denni@192.168.0.32>
2017-12-06 15:11:17 +0100

    push

--- Aufgabe8/main.js
+++ Aufgabe8/main.js
@@ -1,31 +1,23 @@
 var L08_DOM;
 (function (L08_DOM) {
     window.addEventListener("load", init);
-    var quadrat = [];
     var input1 = prompt("Anzahl zwischen 10  und 100");
-    var input2 = prompt("Farbe,Groesse,Position");
-    alert(saveData(input2));
+    var input2 = prompt("Farbe,Groesse,Abstand rechts, Abstand links, Abstand oben, Abstand unten");
     function init() {
+        let splitted = input2.split(",");
         for (let i = 0; i < parseInt(input1); i++) {
             let h = document.createElement("div");
             //console.log(h);
             h.style.position = "absolute";
-            // h.style.top = "50px";
-            h.style.height = "40px";
-            h.style.width = "40px";
-            h.style.backgroundColor = "red";
+            h.style.right = splitted[2];
+            h.style.left = splitted[3];
+            h.style.top = splitted[4];
+            h.style.bottom = splitted[5];
+            h.style.height = splitted[1];
+            h.style.width = splitted[1];
+            h.style.backgroundColor = splitted[0];
             document.body.appendChild(h);
         }
-    }
-    function saveData(_input) {
-        let splitted = _input.split(",");
-        let quadrati = {
-            Farbe: splitted[0],
-            Groesse: parseInt(splitted[1]),
-            Position: parseInt(splitted[2])
-        };
-        quadrat.push(quadrati);
-        return "Thank you";
     }
 })(L08_DOM || (L08_DOM = {}));
 //# sourceMappingURL=main.js.map
--- Aufgabe8/main.ts
+++ Aufgabe8/main.ts
@@ -2,44 +2,29 @@ namespace L08_DOM {
 
     window.addEventListener("load", init);
 
-    interface Quadrat {
-        Farbe: string;
-        Groesse: number;
-        Position: number;
-    }
-    var quadrat: Quadrat[] = [];
 
     var input1: string = prompt("Anzahl zwischen 10  und 100");
 
-    var input2: string = prompt("Farbe,Groesse,Position");
-    alert(saveData(input2));
+    var input2: string = prompt("Farbe,Groesse,Abstand rechts, Abstand links, Abstand oben, Abstand unten");
+
 
     function init(): void {
+        let splitted: string[] = input2.split(",");
         for (let i: number = 0; i < parseInt(input1); i++) {
             let h: HTMLElement = document.createElement("div");
             //console.log(h);
             h.style.position = "absolute";
-            // h.style.top = "50px";
-            h.style.height = "40px";
-            h.style.width = "40px";
-            h.style.backgroundColor = "red";
+            h.style.right = splitted[2];
+            h.style.left = splitted[3];
+            h.style.top = splitted[4];
+            h.style.bottom = splitted[5];
+            h.style.height = splitted[1];
+            h.style.width = splitted[1];
+            h.style.backgroundColor = splitted[0];
             document.body.appendChild(h);
         }
 
 
-    }
-    function saveData(_input: string): string {
-        let splitted: string[] = _input.split(",");
-
-        let quadrati: Quadrat = {
-            Farbe: splitted[0],
-            Groesse: parseInt(splitted[1]),
-            Position: parseInt(splitted[2])
-
-        };
-
-        quadrat.push(quadrati);
-        return "Thank you";
     }
 
 
