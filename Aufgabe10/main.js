c48d63631fd20373882f0c002c93ccfe4e931913
denni <denni@DESKTOP-MEMBM47.eduroam.hs-furtwangen.de>
2017-12-22 10:54:26 +0100

    Dynamisch gemacht

--- Aufgabe10/main.js
+++ Aufgabe10/main.js
@@ -1,5 +1,26 @@
-var L08_DOM;
-(function (L08_DOM) {
-    var preis = [1, 2, 3];
-})(L08_DOM || (L08_DOM = {}));
+var L10_DOM;
+(function (L10_DOM) {
+    window.addEventListener("load", init);
+    function init() {
+        let baum1 = {
+            name: "Baum1",
+            preis: 10
+        };
+        let baum2 = {
+            name: "Baum2",
+            preis: 15
+        };
+        let baum3 = {
+            name: "Baum3",
+            preis: 20
+        };
+        let baeume = [baum1];
+        var fieldset = document.createElement("FIELDSET");
+        var radio = document.createElement("INPUT");
+        radio.setAttribute("type", "radio");
+        radio.setAttribute("name", baeume[0].name);
+        document.body.appendChild(fieldset);
+        fieldset.appendChild(radio);
+    }
+})(L10_DOM || (L10_DOM = {}));
 //# sourceMappingURL=main.js.map
--- Aufgabe10/main.ts
+++ Aufgabe10/main.ts
@@ -1,7 +1,32 @@
-namespace L08_DOM {
+namespace L10_DOM {
+    window.addEventListener("load", init);
+    function init(): void {
+        interface Produkte {
+            name: string;
+            preis: number;
 
-    var preis: number[] = [1, 2, 3];
+        }
 
+        let baum1: Produkte = {
+            name: "Baum1",
+            preis: 10
+        };
+        let baum2: Produkte = {
+            name: "Baum2",
+            preis: 15
+        };
+        let baum3: Produkte = {
+            name: "Baum3",
+            preis: 20
+        };
+        let baeume: Produkte[] = [baum1];
 
+        var fieldset: HTMLElement = document.createElement("FIELDSET");
+        var radio: HTMLElement = document.createElement("INPUT");
+        radio.setAttribute("type", "radio");
+        radio.setAttribute("name", baeume[0].name);
+        document.body.appendChild(fieldset);
+        fieldset.appendChild(radio);
 
+    }
 }
